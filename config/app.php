8d1a7a228fc0c3ffa5599d9b090628de4f16fc4b
putrapangestu <putraprp8@gmail.com>
2026-04-29 11:46:42 +0700

    fix: zlm

--- config/app.php
+++ config/app.php
@@ -123,4 +123,14 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Force HTTPS
+    |--------------------------------------------------------------------------
+    |
+    | Whether to force HTTPS for all requests.
+    |
+    */
+
+    'force_https' => (bool) env('FORCE_HTTPS', false),
 ];
